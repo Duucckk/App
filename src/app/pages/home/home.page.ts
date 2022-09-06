fd9552b5cba39f25f72073917fdd050c11bd9954
Duucckk <pa.fernandezr@duocuc.cl>
2022-09-06 07:31:59 -0300

    ultimos cambios

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@duocu((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,6}))$/;
+  //VAMOS A CREAR EL GRUPO DEL FORMULARIO:
+  alumno = new FormGroup({
+    //Validador de rut TIENE QUE SER UNO EN SERIO SIPO
+    rut : new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,2}.[0-9]{3}.[0-9]{3}-[0-9kK]{1}')]),
+    nom_completo: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    //validador de fecha TIENE QUE SER IGUAL O MAYOR A 17
+    fecha_nac: new FormControl('', Validators.required),
+    semestre: new FormControl('', [Validators.required, Validators.min(1), Validators.max(8)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(18)]),
+    tipo_usuario: new FormControl('alumno'),
+    //Validador de email SIN LA WEA DE DUOC
+    email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern(this.emailPattern)])
+  });
+
+  //VAMOS A CREAR UNA VARIABLE PARA OBTENER LA LISTA DE USUARIOS DEL SERVICIO DE USUARIOS:
+  usuarios: any[] = [];
+  verificar_password: string;
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit() {
+    this.usuarios = this.usuarioService.obtenerUsuarios();
+  }
+
+  //método del formulario
+  registrar(){
+    if (this.alumno.controls.password.value != this.verificar_password) {
+      alert('CONTRASEÑAS NO COINCIDEN!');
+      return;
+    }
+    
+    var registrado: boolean = this.usuarioService.agregarUsuario(this.alumno.value);
+    if (!registrado) {
+      alert('USUARIO YA EXISTE!');
+      return;
+    }
+
+    alert('ALUMNO REGISTRADO!');
+    this.alumno.reset();
+    this.verificar_password = '';
+  }
+
+  eliminar(rutEliminar){
+    this.usuarioService.eliminarUsuario(rutEliminar);
+  }
+
+  buscar(rutBuscar){
+    var alumnoEncontrado = this.usuarioService.obtenerUsuario(rutBuscar);
+    this.alumno.setValue(alumnoEncontrado);
+    this.verificar_password = alumnoEncontrado.password;
+  }
+
+  modificar(){
+    //console.log(this.alumno.value);
+    this.usuarioService.modificarUsuario(this.alumno.value);
+    this.limpiar();
+  }
+
+  limpiar(){
+    this.alumno.reset();
+    this.verificar_password = '';
+  }
+
+}
